37e629b10d17cd8389cd7fc63c6e24006b863819
George Higbie <ghigbie@monigle.com>
2019-01-29 14:32:13 -0700

    Remove welcome page

---
+++ src/components/HooperTest.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="hooper-test">
+
+    </div>
+</template>
+
+<script>
+import  { Hooper, 
+          Slide, 
+          Pagination, 
+          Navigation,
+          Progress } from 'hooper';
+          
+export default {
+    name: 'HooperTest',
+    components: {
+        Hooper,
+        Slide,
+        Pagination,
+        Navigation,
+        Progress 
+    }
+
+}
+</script>
+
+
+<style>
+.hooper-test{
+    background-color: blue;
+}
+</style>
