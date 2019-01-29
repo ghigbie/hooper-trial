28a99e12075530d0d809c94bb3b3509d083a43fc
George Higbie <ghigbie@monigle.com>
2019-01-29 14:33:05 -0700

    Install hooper as npm dep

--- src/components/HooperTest.vue
+++ src/components/HooperTest.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="hooper-test">
-
+        
     </div>
 </template>
 
@@ -10,7 +10,7 @@ import  { Hooper,
           Pagination, 
           Navigation,
           Progress } from 'hooper';
-          
+
 export default {
     name: 'HooperTest',
     components: {
